1610aa4bb46228a3172523dda7a70de129988fb6
Aditya singh <adityasingh14.2019@gmail.com>
2026-07-24 23:07:53 +0530

    *****

--- jio-backend/controllers/UserController.js
+++ jio-backend/controllers/UserController.js
@@ -44,37 +44,100 @@ const getUserWishlist = async (req, res) => {
   }
 };
 
+// const addToWishlist = async (req, res) => {
+//   try {
+//     const userId = req.userId;
+//     const { id, poster_path, name, media_type } = req.body;
+
+//     const user = await UserModel.findById(userId);
+//     if (!user) {
+//       return res.status(404).send("User not found");
+//     }
+
+//     // Duplicate check
+//     if (user.wishlist.find((item) => item.id === id)) {
+//       return res.status(400).json({
+//         message: "Item already in wishlist",
+//         status: "failure",
+//       });
+//     } else {
+//       const wishlistItem = { poster_path, name, id, media_type };
+
+//       // user.wishlist.push(wishlistItem);
+//       // await user.save({ validateBeforeSave: false });
+
+//       await UserModel.findOneAndUpdate(
+//         { _id: userId },
+//         { $push: { wishlist: wishlistItem } },
+//         {
+//           new: true, // Return updated document
+//           upsert: true, // Create document if it doesn't exist
+//         },
+//       );
+
+//       res.status(200).json({
+//         message: "Item added to wishlist",
+//         status: "success",
+//         data: user.wishlist,
+//       });
+//     }
+//   } catch (error) {
+//     res.status(500).json({
+//       message: error.message,
+//       status: "failure",
+//     });
+//   }
+// };
+
 const addToWishlist = async (req, res) => {
   try {
+    // Get authenticated user's id from middleware
     const userId = req.userId;
+
     const { id, poster_path, name, media_type } = req.body;
 
+    // Find user in database
     const user = await UserModel.findById(userId);
+
+    // Return 404 if user does not exist
     if (!user) {
       return res.status(404).send("User not found");
     }
 
-    // Duplicate check
+    // Check if the item already exists in the watchlist
     if (user.wishlist.find((item) => item.id === id)) {
       return res.status(400).json({
-        message: "Item already in wishlist",
+        message: "Item already in watchlist",
         status: "failure",
       });
-    } else {
-      const wishlistItem = { poster_path, name, id, media_type };
-
-      user.wishlist.push(wishlistItem);
-      await user.save({ validateBeforeSave: false });
-
-      res.status(200).json({
-        message: "Item added to wishlist",
-        status: "success",
-        data: user.wishlist,
-      });
     }
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
+
+    // Create watchlist item object
+    const watchlistItem = {
+      poster_path,
+      name,
+      id,
+      media_type,
+    };
+
+    // Add item to watchlist in database
+    await UserModel.findOneAndUpdate(
+      { _id: userId },
+      { $push: { wishlist: watchlistItem } },
+      {
+        new: true, // Return updated document
+        upsert: true, // Create document if it doesn't exist
+      },
+    );
+
+    // Send success response
+    return res.status(200).json({
+      status: "success",
+      message: "Added to Watchlist",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
       status: "failure",
     });
   }
