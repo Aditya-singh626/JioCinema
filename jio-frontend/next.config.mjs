10b3cff41f87139f700b353128dde720ea58709e
Aditya singh <adityasingh14.2019@gmail.com>
2026-08-10 23:29:41 +0530

    editing next.config for  video ,thumbnail and tmdb images

--- jio-frontend/next.config.mjs
+++ jio-frontend/next.config.mjs
@@ -1,8 +1,20 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
-  reactCompiler: true,
   images: {
-    domains: ["image.tmdb.org","localhost"], // ✅ allow TMDB images         
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/api/video/thumbnail/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
+    qualities: [30, 75],
   },
 };
 
